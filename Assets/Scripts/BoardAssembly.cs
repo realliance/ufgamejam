eee18976695fdf8e3175a77adc02eaf29581d68e
Christopher Miller <admin@mctherealm.net>
2018-03-17 09:08:57 -0400

    Added UI functionality

--- Assets/Scripts/BoardAssembly.cs
+++ Assets/Scripts/BoardAssembly.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoardAssembly : MonoBehaviour {
 
@@ -11,14 +12,28 @@ public class BoardAssembly : MonoBehaviour {
 
 	public float firstColumnOffset, columnYoffset, rightLegOffset = 0f;
 
+	public string widthTextPrefix = "Width: ";
+
+	public string heightTextPrefix = "Height: ";
+
+	public Text widthText, heightText;
+
+	void updateText() {
+		widthText.text = widthTextPrefix + column;
+		heightText.text = heightTextPrefix + row;
+	}
+
 	void assembleBoard() {
+		foreach (Transform child in parent.transform) {
+			GameObject.Destroy(child.gameObject);
+		}
 		GameObject leftLeg = (GameObject)Instantiate(leg);
 		leftLeg.transform.parent = parent.transform;
 		leftLeg.transform.localPosition = Vector3.zero;
 		for (int c = 0; c <= column - 1; c++) {
 			GameObject columnObj = new GameObject("Column" + c);
 			columnObj.transform.parent = parent.transform;
-			columnObj.transform.localPosition = new Vector3(firstColumnOffset + c, columnYoffset, 0);
+			columnObj.transform.localPosition = new Vector3(firstColumnOffset + c, columnYoffset * row, 0);
 			for (int r = 0; r <= row - 1; r++) {
 				GameObject rowObj = (GameObject)Instantiate(hole, Vector3.zero, Quaternion.identity);
 				rowObj.transform.parent = columnObj.transform;
@@ -28,6 +43,7 @@ public class BoardAssembly : MonoBehaviour {
 		GameObject rightLeg = (GameObject)Instantiate(leg);
 		rightLeg.transform.parent = parent.transform;
 		rightLeg.transform.localPosition = new Vector3(firstColumnOffset + column + rightLegOffset, 0 , 0);
+		updateText();
 	}
 
 	// Use this for initialization
@@ -35,6 +51,36 @@ public class BoardAssembly : MonoBehaviour {
 		row = 6;
 		column = 7;
 		winNum = 4;
+		assembleBoard();
+	}
+
+	public void AddColumn() {
+		column++;
+		assembleBoard();
+	}
+
+	public void RemoveColumn() {
+		if (column != 1) {
+			column--;
+			assembleBoard();
+		}
+	}
+
+	public void AddRow() {
+		row++;
+		assembleBoard();
+	}
+
+	public void RemoveRow() {
+		if (row != 1) {
+			row--;
+			assembleBoard();
+		}
+	}
+
+	public void RevertToStandard() {
+		row = 6;
+		column = 7;
 		assembleBoard();
 	}
 }
---
+++ Assets/Scripts/ShowCustom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCustom : MonoBehaviour {
+
+	public GameObject width, height;
+
+	public BoardAssembly boardAssembler;
+
+	// Use this for initialization
+	void Start () {
+		width.SetActive(false);
+		height.SetActive(false);
+	}
+
+	public void DropdownChanged(int value) {
+		width.SetActive(value == 1);
+		height.SetActive(value == 1);
+		if (value == 0) {
+			boardAssembler.RevertToStandard();
+		}
+	}
+
+}
