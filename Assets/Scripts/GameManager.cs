92238412c23e35168a32d05d1a81430359d94869
Christopher Miller <admin@mctherealm.net>
2018-03-19 18:17:28 -0400

    Added token gravity and a nice table

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,6 +19,9 @@ public class GameManager : MonoBehaviour {
 
 	bool turnDone = false;
 
+	public float fallingGravity = 9.81f;
+	public float waitUntilFall = 7f;
+
 	public GameObject blueToken, orangeToken;
 	GameObject token;
 	uint currentTokenPosition;
@@ -28,6 +31,8 @@ public class GameManager : MonoBehaviour {
 	public Color32 blue;
 	public string orangesTurn = "Oranges Turn";
 	public Color32 orange;
+
+	List<Rigidbody> tokenRigidbodies = new List<Rigidbody>();
 
 	void updateText() {
 		winText.text = winTextPrefix + winNum;
@@ -71,10 +76,35 @@ public class GameManager : MonoBehaviour {
 		currentTokenPosition = 0;
 	}
 
+
+	float velocity = 0;
+	float startingY = 0;
+	bool fall = false;
+	GameObject newTokenRef;
+
+	void FixedUpdate() {
+		if (fall) {
+			newTokenRef.transform.localPosition = new Vector3(0, Mathf.Lerp(startingY, 0, velocity), 0);
+			velocity += (0.5f * fallingGravity);
+			if (newTokenRef.transform.localPosition.y == 0) {
+				newTokenRef.transform.localPosition = Vector3.zero;
+				velocity = 0;
+				fall = false;
+				newTokenRef = null;
+			}
+		}
+	}
+
 	public void OnMove(uint column, uint row, uint player) {
-		token.transform.parent = boardAssembler.GetHoles()[column, row].transform;
-		token.transform.localPosition = Vector3.zero;
+		tokenRigidbodies.Add(token.GetComponent<Rigidbody>());
+		newTokenRef = token;
 		token = null;
+
+		startingY = newTokenRef.transform.localPosition.y;
+
+		newTokenRef.transform.parent = boardAssembler.GetHoles()[column, row].transform;
+		fall = true;
+
 	}
 
 	public void OnGameWin(uint winningPlayer) {
@@ -85,6 +115,18 @@ public class GameManager : MonoBehaviour {
 	public void OnGameTie() {
 		turnText.text = "Tie!";
 		turnText.color = Color.black;
+	}
+
+	public IEnumerator DelayUntilFall() {
+		yield return new WaitForSeconds(waitUntilFall);
+		foreach(Rigidbody r in tokenRigidbodies) {
+			r.isKinematic = false;
+			yield return new WaitForSeconds(0.05f);
+		}
+	}
+
+	public void OnGameDone(EndState endState, uint? winningPlayer) {
+		StartCoroutine(DelayUntilFall());
 	}
 
 	public void ToggleUI(bool visible) {
@@ -109,6 +151,7 @@ public class GameManager : MonoBehaviour {
 		callbacks.gameVictoryCallback = OnGameWin;
 		callbacks.moveCallback = OnMove;
 		callbacks.gameTieCallback = OnGameTie;
+		callbacks.gameFinishedCallback = OnGameDone;
 		game = new Game(ai, callbacks, boardAssembler.GetColumns(), boardAssembler.GetRows(), winNum, players);
 		ToggleUI(false);
 	}
